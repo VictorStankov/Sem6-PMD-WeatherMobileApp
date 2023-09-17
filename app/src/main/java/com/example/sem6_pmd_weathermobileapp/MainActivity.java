7c16d2da6f3184350a7b86da62cf23012259f373
Victor Stankov <64612982+VictorStankov@users.noreply.github.com>
2023-09-17 17:58:57 +0300

    Update Home Screen (#4)
    * feat: Add image name to imageview tags
    
    * feat: Save information on screen rotation
    
    * fix: Fix crashing when image is not set
    
    * fix: Comment out initial data load (to reduce API usage)
    
    * feat: Ultra-massive commit because I was testing things and I forgot git at all >:(

--- app/src/main/java/com/example/sem6_pmd_weathermobileapp/MainActivity.java
+++ app/src/main/java/com/example/sem6_pmd_weathermobileapp/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.sem6_pmd_weathermobileapp;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -15,11 +18,10 @@ import com.example.sem6_pmd_weathermobileapp.databinding.ActivityMainBinding;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    private String api_url;
-    private String api_token;
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +29,16 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
 
-        api_url = ConfigHelper.getConfigValue(this, "api_url");
-        api_token = ConfigHelper.getConfigValue(this, "api_token");
+        ConfigHelper.getConfigValues(this);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        sharedPreferences = getSharedPreferences("night", 0);
+
+        AppCompatDelegate.setDefaultNightMode(
+                sharedPreferences.getBoolean("night_mode", false) ? AppCompatDelegate.MODE_NIGHT_YES : AppCompatDelegate.MODE_NIGHT_NO
+        );
         setSupportActionBar(binding.appBarMain.toolbar);
 
         DrawerLayout drawer = binding.drawerLayout;
@@ -46,7 +52,9 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-        WeatherHelper.getWeatherInformation(MainActivity.this, api_url, api_token);
+        // Add check for existing data when app is recreated
+        // WeatherHelper.getWeatherInformation(MainActivity.this, api_url, api_token);
+        Log.i("Loaded", "App loaded");
     }
 
     @Override
@@ -66,7 +74,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.refreshButton) {
-            WeatherHelper.getWeatherInformation(MainActivity.this, api_url, api_token);
+            WeatherHelper.getWeatherInformation(MainActivity.this, ConfigHelper.api_url, ConfigHelper.api_token);
+            return true;
+        }
+        if (item.getItemId() == R.id.darkModeButton) {
+            if (sharedPreferences.getBoolean("night_mode", false)) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                sharedPreferences.edit().putBoolean("night_mode", false).apply();
+            }
+            else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                sharedPreferences.edit().putBoolean("night_mode", true).apply();
+            }
             return true;
         }
         return super.onOptionsItemSelected(item);
