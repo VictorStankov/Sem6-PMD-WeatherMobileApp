5ed35f58b9d9ac5d629b3164892d208ac099d0f6
VictorStankov <viktorstankov1@gmail.com>
2023-09-22 17:52:01 +0300

    feat: Add elements to Home View and ViewModel

--- app/src/main/java/com/example/sem6_pmd_weathermobileapp_v2/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/sem6_pmd_weathermobileapp_v2/ui/home/HomeFragment.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.sem6_pmd_weathermobileapp_v2.ConfigHelper;
+import com.example.sem6_pmd_weathermobileapp_v2.MainActivity;
+import com.example.sem6_pmd_weathermobileapp_v2.WeatherHelper;
 import com.example.sem6_pmd_weathermobileapp_v2.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
@@ -24,8 +28,24 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-//        final TextView textView = binding.textHome;
-//        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView conditionText = binding.conditionText;
+        homeViewModel.getCondition().observe(getViewLifecycleOwner(), conditionText::setText);
+
+        final TextView regionCountry = binding.regionCountry;
+        homeViewModel.getRegionCountry().observe(getViewLifecycleOwner(), regionCountry::setText);
+
+        final TextView cityName = binding.cityName;
+        homeViewModel.getCityName().observe(getViewLifecycleOwner(), cityName::setText);
+
+        final TextView currentTemp = binding.currentTemp;
+        homeViewModel.getCurrentTemp().observe(getViewLifecycleOwner(), currentTemp::setText);
+
+        final TextView feelsLikeTemp = binding.feelsLikeTemp;
+        homeViewModel.getFeelsLikeTemp().observe(getViewLifecycleOwner(), feelsLikeTemp::setText);
+
+        final ImageView weatherImage = binding.weatherImage;
+        homeViewModel.getImage().observe(getViewLifecycleOwner(), weatherImage::setImageDrawable);
+
         return root;
     }
 
--- app/src/main/java/com/example/sem6_pmd_weathermobileapp_v2/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/sem6_pmd_weathermobileapp_v2/ui/home/HomeViewModel.java
@@ -1,19 +1,46 @@
 package com.example.sem6_pmd_weathermobileapp_v2.ui.home;
 
+import android.graphics.drawable.Drawable;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class HomeViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private final MutableLiveData<String> regionCountry, cityname, condition, currentTemp, feelsLikeTemp;
+    private final MutableLiveData<Drawable> image;
 
-    public HomeViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+    public MutableLiveData<String> getRegionCountry() {
+        return regionCountry;
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public MutableLiveData<String> getCityName() {
+        return cityname;
+    }
+
+    public MutableLiveData<String> getCondition() {
+        return condition;
+    }
+
+    public MutableLiveData<String> getCurrentTemp() {
+        return currentTemp;
+    }
+
+    public MutableLiveData<String> getFeelsLikeTemp() {
+        return feelsLikeTemp;
+    }
+
+    public MutableLiveData<Drawable> getImage() {
+        return image;
+    }
+
+    public HomeViewModel() {
+        regionCountry = new MutableLiveData<>();
+        cityname = new MutableLiveData<>();
+        condition = new MutableLiveData<>();
+        currentTemp = new MutableLiveData<>();
+        feelsLikeTemp = new MutableLiveData<>();
+        image = new MutableLiveData<>();
     }
 }
