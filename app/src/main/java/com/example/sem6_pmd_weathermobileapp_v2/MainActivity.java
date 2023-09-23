efb4e206e848980490860c4e63af082b2a722d2a
VictorStankov <viktorstankov1@gmail.com>
2023-09-23 19:05:16 +0300

    fix: Remove action bar

--- app/src/main/java/com/example/sem6_pmd_weathermobileapp_v2/MainActivity.java
+++ app/src/main/java/com/example/sem6_pmd_weathermobileapp_v2/MainActivity.java
@@ -43,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-    }
 
+        if (getSupportActionBar() != null)
+            this.getSupportActionBar().hide();
+    }
 }
