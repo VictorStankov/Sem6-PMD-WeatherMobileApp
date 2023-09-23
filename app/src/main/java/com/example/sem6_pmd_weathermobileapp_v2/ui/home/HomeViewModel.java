21c549537ce6209379c295ac66bcd19e50af0b09
VictorStankov <viktorstankov1@gmail.com>
2023-09-23 16:25:06 +0300

    feat: Add list of hourly forecast objects in model view

--- app/src/main/java/com/example/sem6_pmd_weathermobileapp_v2/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/sem6_pmd_weathermobileapp_v2/ui/home/HomeViewModel.java
@@ -5,10 +5,16 @@ import android.graphics.drawable.Drawable;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.sem6_pmd_weathermobileapp_v2.models.HourlyForecast;
+
+import java.util.List;
+
 public class HomeViewModel extends ViewModel {
 
     private final MutableLiveData<String> regionCountry, cityname, condition, currentTemp, feelsLikeTemp, humidity;
     private final MutableLiveData<Drawable> image;
+
+    private final MutableLiveData<List<HourlyForecast>> hourlyForecast;
 
     public MutableLiveData<String> getRegionCountry() {
         return regionCountry;
@@ -38,6 +44,10 @@ public class HomeViewModel extends ViewModel {
         return image;
     }
 
+    public MutableLiveData<List<HourlyForecast>> getHourlyForecast() {
+        return hourlyForecast;
+    }
+
     public HomeViewModel() {
         regionCountry = new MutableLiveData<>();
         cityname = new MutableLiveData<>();
@@ -46,5 +56,6 @@ public class HomeViewModel extends ViewModel {
         feelsLikeTemp = new MutableLiveData<>();
         humidity = new MutableLiveData<>();
         image = new MutableLiveData<>();
+        hourlyForecast = new MutableLiveData<>();
     }
 }
