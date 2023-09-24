a40fd3028cfdc0ecb563363edd6d08e3374e052a
VictorStankov <viktorstankov1@gmail.com>
2023-09-24 16:49:49 +0300

    feat: Move measurement unit parameter in Shared Preferences

--- app/src/main/java/com/example/sem6_pmd_weathermobileapp_v2/ConfigHelper.java
+++ app/src/main/java/com/example/sem6_pmd_weathermobileapp_v2/ConfigHelper.java
@@ -1,6 +1,7 @@
 package com.example.sem6_pmd_weathermobileapp_v2;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.util.Log;
 
@@ -14,6 +15,9 @@ public class ConfigHelper {
     public static String degrees_unit;
 
     public static void getConfigValues(Context context){
+        SharedPreferences sharedPreferences = context.getSharedPreferences(context.getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+        degrees_unit = sharedPreferences.getString("degrees_unit", "C");
+
         Resources resources = context.getResources();
 
         try {
@@ -22,7 +26,6 @@ public class ConfigHelper {
             properties.load(rawResource);
             api_url = properties.getProperty("api_url");
             api_token = properties.getProperty("api_token");
-            degrees_unit = properties.getProperty("degrees_unit");
         }
         catch (Resources.NotFoundException e){
             Log.e("Config", "Unable to find config file: " + e.getMessage());
@@ -32,21 +35,9 @@ public class ConfigHelper {
         }
     }
 
-    public static void updateDegreeValue(Context context, String newUnit){
-        Resources resources = context.getResources();
-
-        try {
-            InputStream rawResource = resources.openRawResource(R.raw.config);
-            Properties properties = new Properties();
-            properties.load(rawResource);
-            properties.setProperty("degrees", newUnit);
-            degrees_unit =properties.getProperty("degrees");
-        }
-        catch (Resources.NotFoundException e){
-            Log.e("Config", "Unable to find config file: " + e.getMessage());
-        }
-        catch (IOException e){
-            Log.e("Config", "Unable to open config file.");
-        }
+    public static void updateDegreeValue(Context context, String newUnit) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(context.getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+        sharedPreferences.edit().putString("degrees_unit", newUnit).apply();
+        degrees_unit = sharedPreferences.getString("degrees_unit", "C");
     }
 }
