6dadea028997604c11fd1f4b955f01188334c8dd
VictorStankov <viktorstankov1@gmail.com>
2023-09-24 15:07:28 +0300

    refactor: Rename Notifications screen to Settings

---
+++ app/src/main/java/com/example/sem6_pmd_weathermobileapp_v2/ui/settings/SettingsViewModel.java
@@ -0,0 +1,11 @@
+package com.example.sem6_pmd_weathermobileapp_v2.ui.settings;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SettingsViewModel extends ViewModel {
+
+    public SettingsViewModel() {
+    }
+}
